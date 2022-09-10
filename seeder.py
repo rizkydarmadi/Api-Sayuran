cda971e95f62d889be32f1579b203782b83fd454
rizkydarmadi69 <rizkydarmadi69@gmail.com>
2022-09-11 02:23:54 +0700

    add data hasil pertanian

--- seeder.py
+++ seeder.py
@@ -47,8 +47,33 @@ class Seeder:
                 value=i['value'],
                 date=datetime(year=2021,month=1,day=1)
             )
+    
+    def seed_sayuran_2020():
+        file = open('data_sayuran/dataset/2020.json')
+        data = json.load(file)
+        data_json = []
+        provinsi_id = 0
+        for index,i in enumerate(data):
+            provinsi_id += 1
+            z = 0
+            for j in data[index]:
+                data_json.append({
+                    "provinsi_id":provinsi_id,
+                    'sayuran_id':int(j),
+                    'value':i[f'{j}']
+                })
+        for i in data_json:
+            if i['value'] == "-":
+                i['value'] = 0
+            Data_sayuranRepository.create_new_data_sayuran(
+                sayuran_id=i['sayuran_id'],
+                provinsi_id=i['provinsi_id'],
+                value=i['value'],
+                date=datetime(year=2020,month=1,day=1)
+            )
         
-Seeder.seed_sayuran_2021()
 # Seeder.seed_provinsi()
 # Seeder.seed_sayuran()
+#Seeder.seed_sayuran_2021()
+Seeder.seed_sayuran_2020()
 
