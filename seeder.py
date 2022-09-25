7b5bb8ede78eee9f8d53747660c48aca1befca5b
rizkydarmadi69 <rizkydarmadi69@gmail.com>
2022-09-25 13:50:08 +0700

    add more year hasil pertanian

--- seeder.py
+++ seeder.py
@@ -285,6 +285,261 @@ class Seeder:
 				value=i['value'],
 				date=datetime(year=2011,month=1,day=1)
 			)
+	
+	def seed_sayuran_2010():
+		file = open('data_sayuran/dataset/2010.json')
+		data = json.load(file)
+		data_json = []
+		provinsi_id = 0
+		for index,i in enumerate(data):
+			provinsi_id += 1
+			z = 0
+			for j in data[index]:
+				data_json.append({
+					"provinsi_id":provinsi_id,
+					'sayuran_id':int(j),
+					'value':i[f'{j}']
+				})
+		for i in data_json:
+			if i['value'] == "-":
+				i['value'] = 0
+			Data_sayuranRepository.create_new_data_sayuran(
+				sayuran_id=i['sayuran_id'],
+				provinsi_id=i['provinsi_id'],
+				value=int(i['value']),
+				date=datetime(year=2010,month=1,day=1)
+			)
+	
+	def seed_sayuran_2009():
+		file = open('data_sayuran/dataset/2009.json')
+		data = json.load(file)
+		data_json = []
+		provinsi_id = 0
+		for index,i in enumerate(data):
+			provinsi_id += 1
+			z = 0
+			for j in data[index]:
+				data_json.append({
+					"provinsi_id":provinsi_id,
+					'sayuran_id':int(j),
+					'value':i[f'{j}']
+				})
+		for i in data_json:
+			if i['value'] == "-":
+				i['value'] = 0
+			Data_sayuranRepository.create_new_data_sayuran(
+				sayuran_id=i['sayuran_id'],
+				provinsi_id=i['provinsi_id'],
+				value=int(i['value']),
+				date=datetime(year=2009,month=1,day=1)
+			)
+	def seed_sayuran_2008():
+		file = open('data_sayuran/dataset/2008.json')
+		data = json.load(file)
+		data_json = []
+		provinsi_id = 0
+		for index,i in enumerate(data):
+			provinsi_id += 1
+			z = 0
+			for j in data[index]:
+				data_json.append({
+					"provinsi_id":provinsi_id,
+					'sayuran_id':int(j),
+					'value':i[f'{j}']
+				})
+		for i in data_json:
+			if i['value'] == "-":
+				i['value'] = 0
+			Data_sayuranRepository.create_new_data_sayuran(
+				sayuran_id=i['sayuran_id'],
+				provinsi_id=i['provinsi_id'],
+				value=int(i['value']),
+				date=datetime(year=2008,month=1,day=1)
+			)
+	def seed_sayuran_2007():
+		file = open('data_sayuran/dataset/2007.json')
+		data = json.load(file)
+		data_json = []
+		provinsi_id = 0
+		for index,i in enumerate(data):
+			provinsi_id += 1
+			z = 0
+			for j in data[index]:
+				data_json.append({
+					"provinsi_id":provinsi_id,
+					'sayuran_id':int(j),
+					'value':i[f'{j}']
+				})
+		for i in data_json:
+			if i['value'] == "-":
+				i['value'] = 0
+			Data_sayuranRepository.create_new_data_sayuran(
+				sayuran_id=i['sayuran_id'],
+				provinsi_id=i['provinsi_id'],
+				value=int(i['value']),
+				date=datetime(year=2007,month=1,day=1)
+			)
+	def seed_sayuran_2006():
+		file = open('data_sayuran/dataset/2006.json')
+		data = json.load(file)
+		data_json = []
+		provinsi_id = 0
+		for index,i in enumerate(data):
+			provinsi_id += 1
+			z = 0
+			for j in data[index]:
+				data_json.append({
+					"provinsi_id":provinsi_id,
+					'sayuran_id':int(j),
+					'value':i[f'{j}']
+				})
+		for i in data_json:
+			if i['value'] == "-":
+				i['value'] = 0
+			Data_sayuranRepository.create_new_data_sayuran(
+				sayuran_id=i['sayuran_id'],
+				provinsi_id=i['provinsi_id'],
+				value=int(i['value']),
+				date=datetime(year=2006,month=1,day=1)
+			)
+	def seed_sayuran_2005():
+		file = open('data_sayuran/dataset/2005.json')
+		data = json.load(file)
+		data_json = []
+		provinsi_id = 0
+		for index,i in enumerate(data):
+			provinsi_id += 1
+			z = 0
+			for j in data[index]:
+				data_json.append({
+					"provinsi_id":provinsi_id,
+					'sayuran_id':int(j),
+					'value':i[f'{j}']
+				})
+		for i in data_json:
+			if i['value'] == "-":
+				i['value'] = 0
+			Data_sayuranRepository.create_new_data_sayuran(
+				sayuran_id=i['sayuran_id'],
+				provinsi_id=i['provinsi_id'],
+				value=int(i['value']),
+				date=datetime(year=2005,month=1,day=1)
+			)
+	def seed_sayuran_2004():
+		file = open('data_sayuran/dataset/2004.json')
+		data = json.load(file)
+		data_json = []
+		provinsi_id = 0
+		for index,i in enumerate(data):
+			provinsi_id += 1
+			z = 0
+			for j in data[index]:
+				data_json.append({
+					"provinsi_id":provinsi_id,
+					'sayuran_id':int(j),
+					'value':i[f'{j}']
+				})
+		for i in data_json:
+			if i['value'] == "-":
+				i['value'] = 0
+			Data_sayuranRepository.create_new_data_sayuran(
+				sayuran_id=i['sayuran_id'],
+				provinsi_id=i['provinsi_id'],
+				value=int(i['value']),
+				date=datetime(year=2004,month=1,day=1)
+			)
+	def seed_sayuran_2003():
+		file = open('data_sayuran/dataset/2003.json')
+		data = json.load(file)
+		data_json = []
+		provinsi_id = 0
+		for index,i in enumerate(data):
+			provinsi_id += 1
+			z = 0
+			for j in data[index]:
+				data_json.append({
+					"provinsi_id":provinsi_id,
+					'sayuran_id':int(j),
+					'value':i[f'{j}']
+				})
+		for i in data_json:
+			if i['value'] == "-":
+				i['value'] = 0
+			Data_sayuranRepository.create_new_data_sayuran(
+				sayuran_id=i['sayuran_id'],
+				provinsi_id=i['provinsi_id'],
+				value=int(i['value']),
+				date=datetime(year=2003,month=1,day=1)
+			)
+	def seed_sayuran_2002():
+		file = open('data_sayuran/dataset/2002.json')
+		data = json.load(file)
+		data_json = []
+		provinsi_id = 0
+		for index,i in enumerate(data):
+			provinsi_id += 1
+			z = 0
+			for j in data[index]:
+				data_json.append({
+					"provinsi_id":provinsi_id,
+					'sayuran_id':int(j),
+					'value':i[f'{j}']
+				})
+		for i in data_json:
+			if i['value'] == "-":
+				i['value'] = 0
+			Data_sayuranRepository.create_new_data_sayuran(
+				sayuran_id=i['sayuran_id'],
+				provinsi_id=i['provinsi_id'],
+				value=int(i['value']),
+				date=datetime(year=2002,month=1,day=1)
+			)
+	def seed_sayuran_2001():
+		file = open('data_sayuran/dataset/2001.json')
+		data = json.load(file)
+		data_json = []
+		provinsi_id = 0
+		for index,i in enumerate(data):
+			provinsi_id += 1
+			z = 0
+			for j in data[index]:
+				data_json.append({
+					"provinsi_id":provinsi_id,
+					'sayuran_id':int(j),
+					'value':i[f'{j}']
+				})
+		for i in data_json:
+			if i['value'] == "-":
+				i['value'] = 0
+			Data_sayuranRepository.create_new_data_sayuran(
+				sayuran_id=i['sayuran_id'],
+				provinsi_id=i['provinsi_id'],
+				value=int(i['value']),
+				date=datetime(year=2001,month=1,day=1)
+			)
+	def seed_sayuran_2000():
+		file = open('data_sayuran/dataset/2000.json')
+		data = json.load(file)
+		data_json = []
+		provinsi_id = 0
+		for index,i in enumerate(data):
+			provinsi_id += 1
+			z = 0
+			for j in data[index]:
+				data_json.append({
+					"provinsi_id":provinsi_id,
+					'sayuran_id':int(j),
+					'value':i[f'{j}']
+				})
+		for i in data_json:
+			if i['value'] == "-":
+				i['value'] = 0
+			Data_sayuranRepository.create_new_data_sayuran(
+				sayuran_id=i['sayuran_id'],
+				provinsi_id=i['provinsi_id'],
+				value=int(i['value']),
+				date=datetime(year=2000,month=1,day=1)
+			)	
 
 # Seeder.seed_provinsi()
 # Seeder.seed_sayuran()
@@ -298,4 +553,16 @@ class Seeder:
 # Seeder.seed_sayuran_2014()
 # Seeder.seed_sayuran_2013()
 # Seeder.seed_sayuran_2012()
-# Seeder.seed_sayuran_2011()
+Seeder.seed_sayuran_2010()
+Seeder.seed_sayuran_2009()
+Seeder.seed_sayuran_2008()
+Seeder.seed_sayuran_2007()
+Seeder.seed_sayuran_2006()
+Seeder.seed_sayuran_2005()
+Seeder.seed_sayuran_2004()
+Seeder.seed_sayuran_2003()
+Seeder.seed_sayuran_2004()
+Seeder.seed_sayuran_2003()
+Seeder.seed_sayuran_2002()
+Seeder.seed_sayuran_2001()
+Seeder.seed_sayuran_2000()
